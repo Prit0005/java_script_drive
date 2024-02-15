918ec15f9896aae92925862e49bc5b259ae8f240
pritesh <senjaliyapritesh37@gmail.com>
2024-02-15 09:41:39 +0530

    Complete

--- Estatein/script.js
+++ Estatein/script.js
@@ -96,3 +96,14 @@ document.querySelectorAll('.nav').forEach(n => n.addEventListener('click', () =>
     Hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }));
+
+
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav');
+    if (window.scrollY > 50) { // Change 50 to the number of pixels scroll you want
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
